71b85a07157cb54acc6709e1eedc80b4048a27ec
dariofusco <fdario@hotmail.it>
2023-08-24 13:48:38 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import TheFooter from "./components/TheFooter.vue";
+export default {
+    components: {
+        TheHeader,
+        TheFooter,
+    }
+}
+</script>
+
+<template>
+<TheHeader></TheHeader>
+<main></main>
+<TheFooter></TheFooter>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+<header></header>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
