15bfa0338622691fb1fbdcd789777c7e65eb193a
dariofusco <fdario@hotmail.it>
2023-08-25 15:12:08 +0200

    aggiunge chiamata api in base alla selezione

--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -2,14 +2,17 @@
 import axios from "axios";
 
 export default {
+
     data() {
         return {
             cards: [],
+            archetype: [],
+            searchArchetype: "",
         };
     },
     methods: {
         fetchCards() {
-            const url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0";
+            const url = `https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=${this.searchArchetype}`;
             axios
                 .get(url)
                 .then((response) => {
@@ -17,15 +20,29 @@ export default {
                     console.log(response.data.data);
                 });
         },
+        fetchArchetype() {
+            const url = "https://db.ygoprodeck.com/api/v7/archetypes.php";
+            axios
+                .get(url)
+                .then((response) => {
+                    this.archetype = response.data;
+                    console.log(response.data)
+                })
+        }
     },
     mounted() {
-        this.fetchCards();
+        //this.fetchCards();
+        this.fetchArchetype();
     },
 };
 </script>
 
 <template>
     <div class="container py-2">
+        <select class="form-select" v-model="searchArchetype" @click="fetchCards">
+            <option disabled value>Selezione carte da cercare</option>
+            <option v-for="singleArchetype in archetype">{{ singleArchetype.archetype_name }}</option>
+        </select>
         <div class="row">
             <div class="col py-2" v-for="card in cards">
                 <div class="card text-center" style="width: 18rem;" v-for="image in card.card_images">
